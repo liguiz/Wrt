0a3af46ad40886fed13dbeb74e3dd9d99af5a1ed
liguiz <liguizeng@gmail.com>
2014-08-23 19:06:52 +0800

    lv

--- src/com/mgoogle/lee/wrt/MainActivity.java
+++ src/com/mgoogle/lee/wrt/MainActivity.java
@@ -3,6 +3,7 @@ package com.mgoogle.lee.wrt;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ListView lv = (ListView)findViewById(R.id.listView1);
+
 	}
 
 	@Override
